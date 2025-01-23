8765f96a52b6eee6609dc32620d219626f0571db
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-01-24 02:41:34 +0500

    fixed

--- src/components/main-content-head/index.jsx
+++ src/components/main-content-head/index.jsx
@@ -1,7 +1,31 @@
 import Image from "next/image";
-
+import { useState } from "react";
 import ThemeChanger from "../theme-switcher";
+
+import useGetQuery from "../../hooks/api/useGetQuery";
+import { KEYS } from "../../constants/key";
+import { URLS } from "../../constants/url";
+import storage from "../../services/storage";
+import { get } from "lodash";
+import { useRouter } from "next/router";
 const MainContentHead = () => {
+  const router = useRouter();
+  const {
+    data: studentProfile,
+    isLoading,
+    isFetching,
+  } = useGetQuery({
+    key: KEYS.studentProfile,
+    url: URLS.studentProfile,
+    headers: {
+      Authorization: `Bearer ${storage.get("authToken")}`,
+    },
+  });
+  const [openProfile, setOpenProfile] = useState(false);
+
+  const handleProfile = () => {
+    setOpenProfile(!openProfile);
+  };
   return (
     <div className={"flex justify-between"}>
       <div className={"flex items-center gap-x-[24px]"}>
@@ -24,25 +48,71 @@ const MainContentHead = () => {
         </button>
       </div>
 
-      <div className={"flex items-center gap-x-[24px]"}>
+      <div className={"relative flex items-center gap-x-[24px]"}>
         <ThemeChanger />
-        <button>
-          <Image
-            src={"/icons/lang.svg"}
-            alt={"sidebar"}
-            width={24}
-            height={24}
-          />
+
+        <button onClick={handleProfile}>
+          <Image src={"/images/user.png"} alt={"user"} width={32} height={32} />
         </button>
 
-        <button>
-          <Image
-            src={"/images/user.png"}
-            alt={"sidebar"}
-            width={32}
-            height={32}
-          />
-        </button>
+        {openProfile && (
+          <div className="absolute bg-white dark:bg-[#26334A] border rounded-md  min-w-[300px] -bottom-[300px] shadow-lg -left-[200px] p-[30px] z-50">
+            <div className="flex gap-x-[12px]">
+              <Image
+                src={"/icons/user.svg"}
+                alt={"user"}
+                width={70}
+                height={70}
+              />
+
+              <div className="space-y-[4px] text-black dark:text-white">
+                <h4 className="">{get(studentProfile, "data.full_name")}</h4>
+                <p className="text-sm">
+                  {get(studentProfile, "data.class_name")}
+                </p>
+                <div className="flex gap-x-[4px]">
+                  <Image
+                    src={"/icons/mail.svg"}
+                    alt={"mail"}
+                    width={18}
+                    height={18}
+                  />
+                  <p className="text-xs text-[#7C8FAC]">
+                    {get(studentProfile, "data.email")}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full h-[1px] bg-[#EAEFF4] rounded-[4px] my-[15px]"></div>
+
+            <button
+              onClick={() => router.push("/student-profile")}
+              className="flex gap-x-[12px] text-start cursor-pointer"
+            >
+              <div className="bg-[#ECF2FF] p-[12px] rounded-md inline-block">
+                <Image
+                  src={"/icons/user-settings.svg"}
+                  alt={"user-settings"}
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <div>
+                <p className="text-black dark:text-white font-semibold">
+                  Mening sahifam
+                </p>
+                <p className="text-[#7C8FAC] text-sm">Sozlamalar</p>
+              </div>
+            </button>
+
+            <div className="w-full h-[1px] bg-[#EAEFF4] rounded-[4px] my-[15px]"></div>
+
+            <button className=" text-[#FA896B] py-[8px] w-full border border-[#FA896B] rounded-md hover:bg-[#FA896B] hover:text-white transform duration-200">
+              Chiqish
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/pages/student-profile/index.js
@@ -0,0 +1,158 @@
+import Dashboard from "@/components/dashboard";
+import Image from "next/image";
+import useGetQuery from "@/hooks/api/useGetQuery";
+import { KEYS } from "@/constants/key";
+import { URLS } from "@/constants/url";
+import storage from "@/services/storage";
+import { get } from "lodash";
+import Link from "next/link";
+import dayjs from "dayjs";
+const Index = () => {
+  const {
+    data: studentProfile,
+    isLoading,
+    isFetching,
+  } = useGetQuery({
+    key: KEYS.studentProfile,
+    url: URLS.studentProfile,
+    headers: {
+      Authorization: `Bearer ${storage.get("authToken")}`,
+    },
+  });
+  return (
+    <Dashboard>
+      <div
+        className={` p-[30px] bg-[#EBF3FE] dark:bg-[#26334AFF]  my-[30px] rounded-[12px]   relative h-[125px] `}
+      >
+        <div className={"space-y-[15px]"}>
+          <p className={"text-[18px] dark:text-white text-black font-semibold"}>
+            Foydalanuvchi profili
+          </p>
+
+          <div className="flex gap-x-[12px] items-center">
+            <Link href={"/"} className="text-[#5A6A85BF]">
+              Bosh sahifa
+            </Link>
+            <div className="bg-black w-[6px] h-[6px] rounded-full  dark:bg-white"></div>
+            <p className="text-black dark:text-white">Mening sahifam</p>
+          </div>
+        </div>
+
+        <div className={"absolute right-[40px] bottom-0"}>
+          <Image
+            src={"/icons/user-profile-bg.svg"}
+            alt={"user-profile-bg"}
+            width={168}
+            height={165}
+          />
+        </div>
+      </div>
+
+      <div className="my-[30px]  grid grid-cols-12 gap-x-[30px]">
+        <div className="col-span-9 bg-[#EBF3FE] dark:bg-[#26334AFF]  border border-[#EAEFF4] dark:border-[#2A3447FF] rounded-md">
+          <div className="flex justify-between items-center p-[30px]">
+            <h3 className="capitalize text-lg font-semibold text-black dark:text-white">
+              tafsilotlar
+            </h3>
+            <ul className="flex gap-x-[15px]">
+              <li>
+                {" "}
+                <Image
+                  src={"/icons/Edit.svg"}
+                  alt={"edit"}
+                  width={24}
+                  height={24}
+                />
+              </li>
+
+              <li>
+                {" "}
+                <Image
+                  src={"/icons/view.svg"}
+                  alt={"edit"}
+                  width={24}
+                  height={24}
+                />
+              </li>
+
+              <li>
+                {" "}
+                <Image
+                  src={"/icons/Share.svg"}
+                  alt={"Share"}
+                  width={24}
+                  height={24}
+                />
+              </li>
+            </ul>
+          </div>
+
+          <div className="w-full h-[1px] bg-[#EAEFF4] mb-[30px]"></div>
+
+          <div className="p-[30px]">
+            <div className="flex gap-x-[12px]">
+              <Image
+                src={"/icons/user.svg"}
+                alt={"user"}
+                width={70}
+                height={70}
+              />
+
+              <div className="space-y-[4px] text-black dark:text-white">
+                <h4 className="">{get(studentProfile, "data.full_name")}</h4>
+                <p className="text-sm text-[#7C8FAC]">
+                  Ta'lim dargohi:{" "}
+                  <span className="capitalize">
+                    {get(studentProfile, "data.academy_or_school")}
+                  </span>
+                </p>
+                <p className="text-sm text-[#7C8FAC]">
+                  {get(studentProfile, "data.class_name")}
+                </p>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-3 gap-[20px] my-[30px] text-black dark:text-white">
+              <div className="col-span-1">
+                <p className="text-sm text-[#7C8FAC]">Telefon raqam</p>
+                <p>+{get(studentProfile, "data.phone")}</p>
+              </div>
+
+              <div className="col-span-1">
+                <p className="text-sm text-[#7C8FAC]">Elektron pochta</p>
+                <p>{get(studentProfile, "data.email")}</p>
+              </div>
+
+              <div className="col-span-1">
+                <p className="text-sm text-[#7C8FAC]">Tug&apos;ilgan sanasi</p>
+                <p>
+                  {dayjs(get(studentProfile, "data.brithday")).format(
+                    "DD.MM.YYYY"
+                  )}
+                </p>
+              </div>
+
+              <div className="col-span-1">
+                <p className="text-sm text-[#7C8FAC]">Viloyati</p>
+                <p>{get(studentProfile, "data.region")}</p>
+              </div>
+
+              <div className="col-span-1">
+                <p className="text-sm text-[#7C8FAC]">Tuman/shahar</p>
+                <p>{get(studentProfile, "data.districts")}</p>
+              </div>
+
+              <div className="col-span-3">
+                <p className="text-sm text-[#7C8FAC]">Manzil</p>
+
+                <p>{get(studentProfile, "data.address")}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Dashboard>
+  );
+};
+
+export default Index;
