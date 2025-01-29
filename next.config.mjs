e04859558747acf66eac6069a984a511a6670cae
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-01-29 16:03:16 +0500

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // permanently
 
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/auth/login",
-        permanent: true,
-        basePath: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/auth/login",
+  //       permanent: true,
+  //       basePath: false,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
