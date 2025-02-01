9fa829334fd04064aec202b75aec0cb9d1960eec
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-02-01 16:40:35 +0500

    fixed

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 
 const Modal = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef(null);
 
   useEffect(() => {
     // Check if the modal has been shown before
@@ -13,6 +14,22 @@ const Modal = () => {
     }
   }, []);
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [isOpen]);
+
   const closeModal = () => {
     setIsOpen(false); // Simply close the modal
   };
@@ -22,7 +39,10 @@ const Modal = () => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
-      <div className="bg-white p-5 rounded-lg relative text-center">
+      <div
+        ref={modalRef}
+        className="bg-white p-5 rounded-lg relative text-center"
+      >
         <button
           onClick={closeModal}
           className="absolute top-2 right-2 bg-transparent border-none text-xl cursor-pointer"
