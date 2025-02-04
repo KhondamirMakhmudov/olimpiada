a65736aa440b85907eca6b95b1e6f53da22b41c0
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-02-04 19:45:52 +0500

    fixed

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,5 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-  pages: {
-    signIn: "/", // Redirect to home if not authenticated
-  },
-});
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard", "/olimpiada", "/results"], // Ensure proper array format
+  matcher: ["/dashboard"],
 };
