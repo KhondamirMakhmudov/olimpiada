bc0621d572aeeaa9858b9524f8b710e433941d43
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-02-08 12:34:57 +0500

    fixed

--- src/components/dashboard/main/index.jsx
+++ src/components/dashboard/main/index.jsx
@@ -3,9 +3,9 @@ import MainContentHead from "@/components/main-content-head";
 const MainContent = ({ children, isSidebarOpen, setIsSidebarOpen }) => {
   return (
     <div
-      className={`transition-all duration-300 ${
-        isSidebarOpen ? "lg:ml-[350px]" : "ml-0"
-      } flex-1 p-[30px] lg:ml-[350px]`}
+      className={`transition-all duration-300 flex-1 p-[30px] ${
+        isSidebarOpen ? "lg:ml-[350px]" : "lg:ml-0"
+      }`}
     >
       <MainContentHead toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
       {children}
